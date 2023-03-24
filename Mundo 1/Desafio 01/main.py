a4d1f9b407faf4fe97700ab2fdef18726b37811d
Lucas <lucasemiliano619@gmail.com>
2023-03-24 01:33:46 -0300

    Criação dos Desafios
    Criação dos Desafios 01, 02 e 03.

---
+++ Mundo 1/Desafio 01/main.py
@@ -0,0 +1,4 @@
+# Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
+
+nome = input('Insira o seu nome: ').title()
+print(f'Olá {nome}, seja bem-vindo!')
---
+++ Mundo 1/Desafio 03/main.py
@@ -0,0 +1,8 @@
+# Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+
+print('Insira dois números:')
+n1 = int(input('Número 1: '))
+n2 = int(input('Número 2: '))
+soma = n1 + n2
+
+print(f'{n1} + {n2} = {soma}')
