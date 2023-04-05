a048fb08cbdd45ef3d7136a54b41de965828723f
Lucas <lucasemiliano619@gmail.com>
2023-04-05 01:00:07 -0300

    Criação exercício 19

---
+++ Mundo 1/ex019 - Sorteando um item na lista/README.md
@@ -0,0 +1,2 @@
+# Exercício 19 – Sorteando um item na lista
+ Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
---
+++ Mundo 1/ex019 - Sorteando um item na lista/main.py
@@ -0,0 +1,14 @@
+# Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
+
+import random
+
+aluno = []
+for i in range(4):
+    nome = input(f'Insira o nome do {i+1}° aluno: ').title
+    aluno.append(nome)
+
+sort = random.choice(aluno)
+print(f'O aluno sorteado foi: {sort}')
+
+#sort = random.randint(0, 3)
+#print(f'O aluno sorteado foi: {aluno[sort]}')
